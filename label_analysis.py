4cc4a50b0032e7cb76a9592982fb00d5256ff492
KeTian <ririhedou@gmail.com>
2018-05-15 21:57:40 -0400

    edit analysis

--- label_analysis.py
+++ label_analysis.py
@@ -8,61 +8,73 @@ from map_domain_to_id import domain_id_map
 def read_label_from(f):
     label_map = dict()
     brand_map = collections.defaultdict(list)
-    ff = open(f ,"r")
+    ff = open(f,"r")
+
+    def label_split(l,domain):
+        if len(l) == 1:
+            return l
+        elif len(l) == 2:
+            return l[0]
+        else:
+            print (domain, l)
+            #raw_input()
+            return '1'
+
     for line in ff.readlines():
         line = line.strip()
         if len(line) == 0 or line.startswith('#'):
             continue
         domain = line.split(',')[0]
         l = line.split(',')[-1]
+
+        l = label_split(l,domain)
+
         brand = line.split(',')[1]
         label_map[domain] = l
         brand_map[brand].append([domain,l])
-    return label_map,brand_map
+    return label_map, brand_map
 
 
-def get_false_positive_by_brand(d, brand):
+def get_false_positive_by_brand(d):
     print (len(d))
-    t = sum(1 for o,j in d if j == '1')
+    t = sum(1 for j in d if d[j] == '1')
     print ("1",t)
+
+    t = sum(1 for j in d if d[j] == '2')
+    print ("2",t)
+
+    t = sum(1 for j in d if d[j] == '0')
+    print ("0", t)
+
     #size = size_map[domain_id_map[int(brand)]]
     #print ("SIZE", size)
     #print ((t+0.0)/size)
 
 
+def statistcs_on_classification_predictions(prediction_f):
+    f = open(prediction_f, "r")
+    brand_size_map = collections.defaultdict(list)
+
+    for line in f.readlines():
+        line = line.strip()
+        _brand = line.split("/")[-2]
+        domain_name = line.split("/")[-1].split("..")[0]
+        brand_size_map[_brand].append(domain_name)
+    f.close()
+    keys = brand_size_map.keys()
+    keys.sort()
+    for i in range(766):
+        if not str(i) in keys:
+            print (i, "0")
+        print (i, len(brand_size_map[str(i)]))
+
+    return
+
 if __name__ == "__main__":
-    other_brand = ["677", "676", "94", "111", "151" ,"20"]
 
-    pop_brand = ["509", "243", "436", "567", "293", "219", "19", "209"]
-
-    # BUSINESS
-    busi = ['493', '509', '231', '140', '345', '28', '574', '68', '268', '721', '762', '582', '119', '589', '632',
-            '566', '36', '307', '127', '609', '102', '248', '80', '731', '334', '742', '357', '550', '56', '700', '239',
-            '20', '716', '490', '433', '636', '253', '356', '206', '149', '651', '352', '545', '760', '360']
-
-    shopping = ['467', '711', '235', '340', '83', '632', '331', '324', '731', '124', '321', '482', '296', '470', '486',
-                '412', '656', '88', '107', '177', '282', '408', '381', '714', '759', '578', '690', '502', '339', '587',
-                '546', '281', '270', '293', '5', '503', '710', '59', '366', '616', '755', '72', '64', '581', '389',
-                '337']
-    #computer
-    com = ['293', '758', '243', '753', '676', '393', '92', '343', '737', '447', '493', '436', '671', '45', '613',
-             '727', '285', '509', '520', '19', '209', '445', '50', '600', '564', '101', '703', '593', '424', '158',
-             '290', '728', '574', '665', '451', '428', '329', '604', '398', '293', '97', '42']
-
-    visited = pop_brand + other_brand
-    visited = list(set(visited))
-
-    f_mb = "/home/ketian/Desktop/phishingdetect/label_tool/SNAP1.MB.label"
-    f_web = "/home/ketian/Desktop/phishingdetect/label_tool/SNAP1.WEB.label"
-    _, mb_brand = read_label_from(f_mb)
-    _, web_brand = read_label_from(f_web)
-
-    for i in busi+com+shopping:
-        if i in visited:
-            continue
-        print (i, domain_id_map[int(i)])
-        get_false_positive_by_brand(web_brand[i], i)
-        print ("-------------")
-        get_false_positive_by_brand(mb_brand[i], i)
-
-        raw_input()
+    #f_mb = "MAY14.SNAP1.MB.label"
+    f_web = "MAY14.SNAP1.WEB.label"
+    #_, mb_brand = read_label_from(f_mb)
+    web_label, _ = read_label_from(f_web)
+    #get_false_positive_by_brand(web_label)
+    statistcs_on_classification_predictions("data_need_label/snap1_retrain.txt")
