34743f269abc9dd0a5a522f9d52fd350fdf82a19
Ke Tian <ketian@kes-MacBook-Air.local>
2018-05-16 01:06:06 -0400

    add label analyssi

--- label_analysis.py
+++ label_analysis.py
@@ -25,71 +25,83 @@ def read_label_from(f):
         if len(line) == 0 or line.startswith('#'):
             continue
         domain = line.split(',')[0]
-        l = line.split(',')[-1]
-
-        l = label_split(l,domain)
-
         brand = line.split(',')[1]
-        label_map[domain] = l
-        brand_map[brand].append([domain,l])
+
+        l = line.split(',')[-1]
+        l = label_split(l,domain)
+        label_map[domain+ ' '+brand] = l
+        brand_map[brand].append(domain)
+
+    print ("TOTAL", sum(len(brand_map[i]) for i in brand_map))
     return label_map, brand_map
 
 
 def get_false_positive_by_brand(d):
     print (len(d))
-    t = sum(1 for j in d if d[j] == '1')
-    print ("1", t)
+    t1 = sum(1 for j in d if d[j] == '1')
+    print ("1", t1)
 
-    t = sum(1 for j in d if d[j] == '2')
-    print ("2", t)
+    t2 = sum(1 for j in d if d[j] == '2')
+    print ("2", t2)
 
-    t = sum(1 for j in d if d[j] == '0')
-    print ("0", t)
+    t3 = sum(1 for j in d if d[j] == '0')
+    print ("0", t3)
 
     #size = size_map[domain_id_map[int(brand)]]
     #print ("SIZE", size)
     #print ((t+0.0)/size)
+    print ("ONLY 1", (t1+0.0)/len(d))
+    print ("1 AND 2",(t1+t2+0.0)/len(d))
 
 
 def statistcs_on_classification_predictions(prediction_f):
     f = open(prediction_f, "r")
-    brand_size_map = collections.defaultdict(list)
+    brand_map = collections.defaultdict(list)
 
     predictions = []
     for line in f.readlines():
         line = line.strip()
         _brand = line.split("/")[-2]
         domain_name = line.split("/")[-1].split("..")[0]
-        brand_size_map[_brand].append(domain_name)
+        brand_map[_brand].append(domain_name)
 
-    for i in brand_size_map:
-        brand_size_map[i] =list(set(brand_size_map[i]))
+    for i in brand_map:
+        brand_map[i] =list(set(brand_map[i]))
 
     f.close()
-    keys = brand_size_map.keys()
+    keys = brand_map.keys()
     keys.sort()
     for i in range(766):
         if not str(i) in keys:
-            print (i,  domain_id_map[i], 0)
+            #print (i,  domain_id_map[i], 0)
             predictions.append([domain_id_map[i], 0])
         else:
-            print (i, domain_id_map[i], len(brand_size_map[str(i)]))
-            predictions.append([domain_id_map[i], len(brand_size_map[str(i)])])
+            #print (i, domain_id_map[i], len(brand_map[str(i)]))
+            predictions.append([domain_id_map[i], len(brand_map[str(i)])])
 
     predictions.sort(key=lambda x: x[1])
     total = sum(i[1] for i in predictions)
 
-    for i in predictions:
-        print (i[0], i[1], float(i[1])/total)
+    #for i in predictions:
+    #    print (i[0], i[1], float(i[1])/total)
 
     print ("TOTAL", total)
-    return
+    print ("TOTAL", sum(len(brand_map[i]) for i in brand_map))
+    return brand_map
+
 
 if __name__ == "__main__":
 
     #f_mb = "MAY14.SNAP1.MB.label"
     f_web = "MAY14.SNAP1.WEB.label"
     #_, mb_brand = read_label_from(f_mb)
-    web_label, _ = read_label_from(f_web)
-    #get_false_positive_by_brand(web_label)
-    statistcs_on_classification_predictions("data_need_label/snap1_retrain.txt")
+    web_label, web_brand_map = read_label_from(f_web)
+    get_false_positive_by_brand(web_label)
+    """
+    web_brand_snap1_map = statistcs_on_classification_predictions("data_need_label/snap1_retrain.txt")
+
+    for i in web_brand_snap1_map:
+        if len(web_brand_snap1_map[i]) != len(web_brand_map[i]):
+            print (i, len(web_brand_snap1_map[i]))
+    """
+
